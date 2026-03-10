8a4abddb2c9d02dee8bc6dd5bb8e64d15301da97
anuj solania <anujsolania0507@gmail.com>
2026-03-10 21:53:03 +0530

    footer text updated

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,18 @@
-
-import Body from "../components/Body"
-import Navbar from "../components/Navbar"
-
+import Body from "../components/Body";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-
-
   return (
-    <div className="bg-gray-300 min-h-screen w-screen flex flex-col" >
-      <Navbar/>
-      <div className="grow" >
-      <Body></Body>
+    <div className="bg-gray-300 min-h-screen w-screen flex flex-col">
+      <Navbar />
+      <div className="grow">
+        <Body></Body>
       </div>
-      <div className="bg-[#50A2FF] font-serif text-center p-2" >
-        © 2025 Docify. All rights Reserved
+      <div className="bg-[#50A2FF] font-serif text-center p-2">
+        © 2026 RealTimeDocs. Released under the MIT License.
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
